b7f7a0855ae209a01dd2b98d0017b897e924d737
Bawantha Lahiru <bawanthalahiru125@gmail.com>
2025-06-18 10:57:10 +0530

    Minor Changes

--- src/components/Mill/header.jsx
+++ src/components/Mill/header.jsx
@@ -4,12 +4,18 @@ export default function Header() {
   return (
     <header className="bg-[#d9d9d9] shadow h-[70px]">
       <div className="mx-auto px-4 py-3 flex">
-        <div className="w-[35px] h-[35px] bg-[#A86523] border border-black mx-5 rounded-sm ">Logo</div>
+        <div className="w-[45px] h-[45px] mx-2">
+          <img src="/logo.png" alt="Logo" />
+        </div>
         <div className="m-0 p-0">
           <h1 className="text-3xl font-poppins font-bold m-0 p-0">WoodWise</h1>
-          <div className="flex justify-end"><h2 className="text-xl font-semibold m-0 p-0">Mill</h2></div>
+          <div className="flex justify-end">
+            <h2 className="text-xl font-semibold m-0 p-0">Mill</h2>
+          </div>
         </div>
-        <div className="ml-auto"><HeaderButtons/></div>
+        <div className="ml-auto">
+          <HeaderButtons />
+        </div>
       </div>
     </header>
   );
