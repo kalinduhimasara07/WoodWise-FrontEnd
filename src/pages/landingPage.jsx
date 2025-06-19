83a27ae088cb2c5323728ed390e985adcb29274b
Ravindu Nirman <ravindunirman3@gmail.com>
2025-06-19 10:17:40 +0530

    Route fix

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -3,14 +3,18 @@ import ImageSlider from "../components/Home/imageSlider";
 import FurnitureSlider from "../components/Home/productSlider";
 import VideoComponent from "../components/Home/video";
 import WoodenFurnitureFeatures from "../components/Home/wodenFurnitureFeatures";
+import Header from "../components/Home/header";
+import Footer from "../components/Home/footer";
 
 export default function LandingPage() {
   return (
     <div>
+      <Header />
       <ImageSlider />
       <FurnitureSlider />
       <WoodenFurnitureFeatures/>
       <VideoComponent />
+      <Footer />
     </div>
   );
 }
