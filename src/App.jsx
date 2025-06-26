bcc3aaa02ab5d4087fb779d79a654e68e2c4429d
Ravindu Nirman <ravindunirman3@gmail.com>
2025-06-26 15:59:36 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StoreDashboard from "./pages/Store/storeDashboard";
 import MillDashboard from "./pages/Mill/millDashboard";
 import LoginPage from "./pages/login";
-import HomePage from "./pages/homePage";
 import AddFurniture from "./pages/Admin/addFurniture";
 import AddUser from "./pages/Admin/addUser";
 import LandingPage from "./pages/landingPage";
@@ -21,7 +20,6 @@ function App() {
           <Route path="/admin/add-furniture" element={<AddFurniture />} />
           <Route path="/admin/add-user" element={<AddUser />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="*" element={<HomePage />} />
           <Route path="/overview" element={<ProductOverview />} />
           <Route
             path="/furniture"
